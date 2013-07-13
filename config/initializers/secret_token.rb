a04f42f6b1937423438e4c2f19d7639f500cca33
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-07-13 15:24:08 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modufy::Application.config.secret_token = '16adada6b8c3f7b7129470e4fb1faeaaf27c6ebcf3d22b804d3ee469f0b371ee6901d904442a50c0c3eb9624c87e8ec9a1f8015526556b16922c3f409cac34fa'
